7b00bdc6de0214c6c4051eebe395146f6febb2f8
1800steemers <juliangarc0575@gmail.com>
2016-11-16 14:01:59 -0800

    done with board starting game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,6 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function topleft() {
+  var Shape= document.createElementNS(namespace, "circle")
+}
