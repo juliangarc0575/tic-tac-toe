7f300fd00a5400679aea0d9be9ed3f9bd530459e
1800steemers <juliangarc0575@gmail.com>
2016-11-18 13:52:54 -0800

    ayeeee almost done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,11 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 function topleft() {
-  var Shape= document.createElementNS(namespace, "circle")
+  var topleft= document.createElementNS(namespace, "circle")
   var canvas=document.getElementById("game-board")
-  
+  topleft.setAttribute("cx", 45)
+  topleft.setAttribute("cy", 50)
+  topleft.setAttribute("r", 30)
+  topleft.setAttribute("fill", "green")
+  canvas.appendChild(topleft)
 }
