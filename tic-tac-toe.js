b37085610d093ca179a17856e770222e8b648c6a
1800steemers <juliangarc0575@gmail.com>
2016-11-28 18:51:57 -0800

    ugh

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,48 +1,45 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
+var player1 = "circle"
 function topleft() {
-  var topleft= document.createElementNS(namespace, "circle")
-  var canvas=document.getElementById("game-board")
-  topleft.setAttribute("cx", 45)
-  topleft.setAttribute("cy", 50)
-  topleft.setAttribute("r", 30)
-  topleft.setAttribute("fill", "green")
-  canvas.appendChild(topleft)
+  var canvas = document.getElementById("game-board")
+  if (player1 == "circle") {
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx", 45)
+      circle.setAttribute("cy", 50)
+        circle.setAttribute("r", 20)
+        circle.setAttribute("fill", "magenta")
+        canvas.appendChild(circle)
+        player1 = "rect"
+  } else {
+    var rect = document.createElementNS(namespace, "rect")
+    rect.setAttribute("x", 45)
+    rect.setAttribute("y", 50)
+    rect.setAttribute("fill", "blue")
+    canvas.appendChild(rect)
+    player1 = "circle"
+  }
 }
+var player1 = "rect"
 function topmid() {
-  var topmid= document.createElementNS(namespace, "circle")
-  var canvas=document.getElementById("game-board")
-  topmid.setAttribute("cx", 135)
-  topmid.setAttribute("cy", 45)
-  topmid.setAttribute("r", 30)
-  topmid.setAttribute("fill", "green")
-  canvas.appendChild(topmid)
-}
-function topright() {
-  var topright= document.createElementNS(namespace, "circle")
-  var canvas=document.getElementById("game-board")
-  topright.setAttribute("cx", 225)
-  topright.setAttribute("cy", 45)
-  topright.setAttribute("r", 30)
-  topright.setAttribute("fill", "green")
-  canvas.appendChild(topright)
-}
-function midleft() {
-  var midleft= document.createElementNS(namespace, "circle")
-  var canvas=document.getElementById("game-board")
-  midleft.setAttribute("cx", 45)
-  midleft.setAttribute("cy", 140)
-  midleft.setAttribute("r", 30)
-  midleft.setAttribute("fill", "green")
-  canvas.appendChild(midleft)
-}
-function midmid() {
-  var midmid= document.createElementNS(namespace, "circle")
-  var canvas=document.getElementById("game-board")
-  midmid.setAttribute("cx", 135)
-  midmid.setAttribute("cy", 140)
-  midmid.setAttribute("r", 30)
-  midmid.setAttribute("fill", "green")
-  canvas.appendChild(midmid)
+  var canvas = document.getElementById("game-board")
+  if (player1 == "circle") {
+    var circle = document.createElementNS(namespce, "circle")
+    circle.setAttribute("cx", 140)
+    circle.setAttribute("cy", 50)
+    circle.setAttribute("r", 20)
+    circle.setAttribute("fill", "magenta")
+    circle.appendChild(circle)
+    player1 = "rect"
+
+  }else {
+    var rect = docuument.createElementNS(namespace, "rect")
+    rect.setAttribute("x", 140)
+    rect.setAttribute("y", 50)
+    rect.setAttribute("fill", "blue")
+    rect.appendChild(rect)
+    player1 = "circle"
+  }
 }
